fbd5e8ca33e06d55dca06c142f2ae65062d98a03
Marc Zaku <mzaku@hotmail.de>
2019-12-14 13:57:05 +0100

    ide and engine upgrade and api improvement

--- Assets/Source/Mediabox/GameKit/Game/GameWithoutSaveGame.cs
+++ Assets/Source/Mediabox/GameKit/Game/GameWithoutSaveGame.cs
@@ -3,7 +3,7 @@
 	/// Inherit from this class, if you want to define a game without a savegame. Place it in any relevant scene / prefab that is loaded from GameManager.
 	/// </summary>
 	/// <typeparam name="TGameDefinition">The type of GameDefinition needs to match the one used in your GameManager.</typeparam>
-	public abstract class GameWithoutSavegame<TGameDefinition> : GameBase<TGameDefinition> {
+	public abstract class GameWithoutSaveGame<TGameDefinition> : GameBase<TGameDefinition> {
 		public override void Save(string path) { }
 		public override void Load(string path) { }
 	}
