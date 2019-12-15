ae0cf1384aa7ee1e48a9ea9e2e13a4521aa004e1
Marc Zaku <mzaku@hotmail.de>
2019-12-15 13:44:31 +0100

    adds strip engine code and a working build script for an app without crashes.

---
+++ Assets/Source/Mediabox/GameManager/Editor/BuildGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace Mediabox.GameManager.Editor {
+	public static class BuildGame {
+		[MenuItem("MediaBox/Build Game")]
+		public static void Build() {
+			var buildPlayerOptions = BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(new BuildPlayerOptions());
+			buildPlayerOptions.assetBundleManifestPath = "AssetBundles/iOS/iOS.manifest";
+			BuildPipeline.BuildPlayer(buildPlayerOptions);
+		}
+	}
+}
