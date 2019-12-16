74c6e0b4dd9a2c08f1cd5107009f3216f3fdfd44
Daniel Matzke (mac) <dm@leetvision.com>
2019-12-16 12:42:27 +0100

    added code to catch an exception when cancelling build target path picker window

--- Assets/Source/Mediabox/GameManager/Editor/BuildGame.cs
+++ Assets/Source/Mediabox/GameManager/Editor/BuildGame.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEditor;
+using UnityEngine;
 
-namespace Mediabox.GameManager.Editor {
-	public static class BuildGame {
-		[MenuItem("MediaBox/Build Game")]
-		public static void Build() {
-			var buildPlayerOptions = BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(new BuildPlayerOptions());
-			buildPlayerOptions.assetBundleManifestPath = "AssetBundles/iOS/iOS.manifest";
-			BuildPipeline.BuildPlayer(buildPlayerOptions);
-		}
-	}
+namespace Mediabox.GameManager.Editor
+{
+    public static class BuildGame
+    {
+        [MenuItem("MediaBox/Build Game")]
+        public static void Build()
+        {
+            BuildPlayerOptions buildPlayerOptions;
+            try
+            {
+                buildPlayerOptions = BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(new BuildPlayerOptions());
+            }
+            catch (UnityEditor.BuildPlayerWindow.BuildMethodException e)
+            {
+                Debug.Log("BuildMethodException caught: " + e);
+                return;
+            }
+            buildPlayerOptions.assetBundleManifestPath = "AssetBundles/iOS/iOS.manifest";
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+    }
 }
