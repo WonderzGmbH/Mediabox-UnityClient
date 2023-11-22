a9d36e2ff64e98875ab691636638e82bdaa02328
Marc Zaku <mzaku@hotmail.de>
2023-11-22 02:41:49 +0100

    Update Demo Scene to include a score sample

--- Assets/Plugins/Mediabox/Samples~/GameA/Rotate.cs
+++ Assets/Plugins/Mediabox/Samples~/GameA/Rotate.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Mediabox.Samples.GameA {
-	public class Rotate : MonoBehaviour {
+	public class Rotate : MonoBehaviour, IPointerClickHandler {
 		public Vector3 rotation;
 
 		public void Update() {
 			this.transform.Rotate(this.rotation * Time.deltaTime);
+		}
+
+
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			var game = FindObjectOfType<Game>();
+			game.API.ReportNewUserScore(game.Score+1);
 		}
 	}
 }
